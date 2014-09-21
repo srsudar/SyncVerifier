695babcb0cab51c3e543e2973694d927206bba90
Sam Sudar <sudar.sam@gmail.com>
2014-09-21 17:03:05 -0500

    add util class.

---
+++ app/src/androidTest/java/syncverifier/opendatakit/org/syncverifier/util/SyncVerifierUtil.java
@@ -0,0 +1,35 @@
+package syncverifier.opendatakit.org.syncverifier.util;
+
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+/**
+ * Created by sudars on 9/21/14.
+ */
+public class SyncVerifierUtil {
+
+
+    public static EditText getEditText(View rootView, int id) {
+        EditText result = (EditText) rootView.findViewById(id);
+        return result;
+    }
+
+    public static Spinner getSpinner(View rootView, int id) {
+        Spinner result = (Spinner) rootView.findViewById(id);
+        return result;
+    }
+
+    public static Button getButton(View rootView, int id) {
+        Button result = (Button) rootView.findViewById(id);
+        return result;
+    }
+
+    public static TextView getTextView(View rootView, int id) {
+        TextView result = (TextView) rootView.findViewById(id);
+        return result;
+    }
+
+}
--- app/src/main/java/syncverifier/opendatakit/org/syncverifier/MainFragment.java
+++ app/src/main/java/syncverifier/opendatakit/org/syncverifier/MainFragment.java
@@ -35,7 +35,14 @@ public class MainFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        View rootView = inflater.inflate(
+                R.layout.fragment_main,
+                container,
+                false);
+
+        this.mEnterServerUrl = SyncVe
+
         return rootView;
     }
+
 }
